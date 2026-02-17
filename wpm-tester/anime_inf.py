54416543214862a33e287e7da01314d0fe131dbd
BrishkamalKarki <brishkamalkarki@gmail.com>
2026-02-17 18:06:55 +0545

    initial commit of wpm tester

---
+++ wpm-tester/anime_inf.py
@@ -0,0 +1,39 @@
+import random
+
+def inf_ani():
+    
+    num:int=random.randint(1,10)
+    match num:
+        case 1:
+            return("Humankind cannot gain anything without first giving something in return." \
+            " To obtain, something of equal value must be lost.")
+        case 2:
+            return("Dedicate your heart! The brave ones who are willing to sacrifice their" \
+            " lives can bring about a change that will allow us to stand tall and fight for " \
+            "humanity's freedom.")
+        case 3:
+            return("A true warrior needs no sword. " \
+            "A true warrior is defined by how they protect life, not how they take it.")
+        case 4:
+            return("If you only move on what you think is right, you will never get the truth." \
+            "The truth is often cruel, and people must face it.")
+        case 5:
+            return("I am the strongest because I am Satoru Gojo. " \
+                "You should know your own worth, or you will always be limited by others.")
+        case 6:
+            return("I was born for this, a creature whose sole purpose is to stand above all life. " \
+            "Yet, I found that strength is nothing without knowing who you are protecting.")
+        case 7:
+            return("Whether you win or lose, you can always learn something new by running away. " \
+            "But you have to remember, your life is your own.")
+        case 8:
+            return("Set your heart ablaze. Go beyond your limits. " \
+            "I am the Flame Hashira, and I will protect you all, even in death.")
+        case 9:
+            return("I\'m not going there to die. I\'m going to find out if I\'m really alive. " \
+            "You\'re gonna carry that weight.")
+        case 10:
+            return("If you're going to live, take action. " \
+            "Don't wait for salvation from someone else; you have to choose to survive.")
+    
+    
